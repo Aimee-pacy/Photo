e6d5eba4f65b93e2b49c13fd240dd7785859e7d6
Aimee-pacy <ishimwefique.com>
2021-04-22 18:38:45 +0200

    Removed and added new files

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,24 +1,20 @@
-"""instagram URL Configuration
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url,include
-from django.contrib import admin
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'',include('clone.urls')),
-    url(r'^accounts/', include('registration.backends.simple.urls')),
-
+urlpatterns=[
+    url('^$',views.index,name = 'index'),
+    url(r'^new/upload$', views.upload_image, name='upload'),
+    url(r'^new/profile$', views.add_profile, name='edit'),
+    url(r'^myprofile$', views.my_profile, name='myprofile'),
+    url(r'^comment/(\d+)/$', views.add_comment, name='comment'),
+    url(r'^search/',views.search_users, name = 'searchs'),
+    url(r'^like/(\d+)/$',views.likes, name = 'like'),
+    url(r'^follow/(\d+)/$',views.following, name = 'follow'),
+    url(r'^usersprofile/(\d+)/$', views.users_profile,name='usersprofile'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
