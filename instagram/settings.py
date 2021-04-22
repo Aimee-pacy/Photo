5b6a2738816c0097c9e282c173910e3cea238f87
Aimee-pacy <ishimwefique.com>
2021-04-22 18:28:20 +0200

    New changes on db

--- instagram/settings.py
+++ instagram/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'instagram',
-        'USER': 'beryl',
+        'USER': 'fique',
     'PASSWORD':'123',
     }
 }
